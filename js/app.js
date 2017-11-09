e7a411c7b99c21fa5a3c64f75b497f78779267aa
melissayauri <melissayauri>
2017-11-09 18:20:15 -0500

    incorporando el evento click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
-/*primero se declara las variables, funciones y luego eventos*/
+/* primero se declara las variables, funciones y luego eventos*/
+/* variable para las posiciones del tablero*/
+/* var arrayTictoc = new array(9);*/
+/* variable para cada recuadro del tablero*/
+var square = document.getElementsByClassName('square');
+
+
+/* funciones*/
+/* funcion para validar si hay una combinacion que gano*/
+function winningPlayer() {
+
+}
+
+/* funcion para el juego*/
+function ticToc(evento) {
+  /* alert('primer cuadro');*/
+  /* se enfoca en el elemento id del evento click*/
+  var id = evento.target.id;
+  var squareSelection = id[1] - 1;
+  /* alert(squareSelection);*/
+}
+
+/* funcion para que carge la pagina, o al hacer click*/
+function loadPage() {
+  /* asignando el evento click a cada cuadrado*/
+  /* console.log(document.getElementsByClassName('square'));*/
+  var count = 0;
+  /* recorreiendo cada iteraccion en el cuadrado*/
+  while (count < square.length) {
+    /* console.log(count, square[count]);*/
+    /* se ejecuta cuando se hace click en el cuadrado*/
+    square[count].addEventListener('click', ticToc);
+    count++;
+  }
+}
+
+/* eventos*/
+window.addEventListener('load', loadPage);
+
+/* alert('hola');*/
